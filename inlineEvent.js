03e3d3f6f1e18b7101432bdd7f50fdea2c54243f
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-06 14:14:18 +0500

    Create inlineEvent.js

---
+++ inlineEvent.js
@@ -0,0 +1,10 @@
+// lets talk about inline events in javascript .
+// there are three ways to respond to user events .
+// the one way is adding inline events in javascirpt .
+
+// HTML
+<button onCLick = "alert = ('Stop clicking .........') ; alert('You mother fucker stop clikcing the button....')">Click meee</button>
+
+// its kind of annoyiing juncky and feels bezar to code and handle.
+// inline HTML events are not used in the current era so for learing and starting work with events its great but for day to day use its just trash.
+
