c033f40e9fcd30de22ae81a7d1f3761a29460f78
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-06 15:07:50 +0500

    Update addEventListner.js

--- addEventListner.js
+++ addEventListner.js
@@ -1,1 +1,38 @@
+// addEventListner is the third way of handlying events in javacript.
+
+// addEventListner
+// Specify the event type and a callBack to run.
+
+// Note down one thing is that we dont have more then one event on the 2 way onlcick. 
+// addEventLsitner will resolve this problem now you can add many events as you want.
+
+
+         const imgAll = document.querySelector('img');
+          imgAll.addEventListener('click',function() {
+            alert('Download me from Unsplash');
+          });
+
+
+          const btn = document.querySelector('button');
+          btn.addEventListener('dblclick',function(){
+            alert('Hey How are you ..??')
+          });
+
+
+
+          const btn = document.querySelector('button');
+
+          function single(){
+            alert('Welcome to our website');
+          };
+
+          function double(){
+            alert('Hey How are you ..??')
+          };
+                    
+          btn.addEventListener('click', single);
+          btn.addEventListener('click', double);
+
+
+
 
