d2dd6f033c22fe82a9bfe663721f0bdaae101029
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-09 12:02:44 +0500

    Update EventBubling.js

--- EventBubling.js
+++ EventBubling.js
@@ -1,4 +1,21 @@
 // Lets have a look on how to buble an event in javascript 
 // lets get started.
 
+// HTML
+/*
+
+<body>
+
+<section onclick = 'alert('Section Clicked')'>
+<p onclick = 'alert('paragraph clicked')'>
+<button onclick="alert('button CLicked')">Click me</button>
+</p>
+</section>
+
+</body>
+
+*/
+
+// When you click on the button it will first alert you that the button is clicked and after then paragraph is clicked and after that section clicked means
+// it will not run the oder or step by step one any event is called it triggers all the remeaning events.
 
