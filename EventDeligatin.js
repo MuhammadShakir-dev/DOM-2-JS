4586b59549364ea8c8732a3e318f831ccce8d63b
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-09 12:50:42 +0500

    Update EventDeligatin.js

--- EventDeligatin.js
+++ EventDeligatin.js
@@ -1,2 +1,6 @@
 // lets have a look on our last part of javascript DOM called event Delegation.
+// its a stratergy for writing our code.
 
+                    li.addEventListener('click',function(e){
+                     e.target.nodeName === 'LI' && e.target.remove();  
+                  })
